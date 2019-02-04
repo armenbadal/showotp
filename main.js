250aa1c36f92eb38a4c38355840d6e3698932756
Armen Badalian <abadalia@synaptics.com>
2019-02-04 16:27:18 +0400

    Ծրագրի ֆայլերը

---
+++ main.js
@@ -0,0 +1,57 @@
+
+const electron = require('electron')
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+
+//
+var mainWindow = null
+
+//
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({width: 600, height:400})
+    mainWindow.loadURL('file://' + __dirname + '/index.html')
+
+    //mainWindow.webContents.openDevTools()
+    
+    mainWindow.on('closed', () => { mainWindow = null })
+})
+
+//
+app.on('window-all-closed', () => { app.quit() })
+
+
+//
+const mainMenuTempl = [
+    {
+        label: 'Ծրագիր',
+        submenu: [
+            {
+                label: 'Ընտրել ֆայլ...',
+                click() { mainWindow.webContents.send('read-data-file') }
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Ելք',
+                click() { app.quit() }
+            }
+        ]
+    },
+    {
+        label: 'Յուշում',
+        submenu: [
+            {
+                label: 'Տեղեկութիւն',
+                click() { mainWindow.webContents.send('show-info') }
+            }
+        ]
+    }
+]
+
+//
+const mainMenu = Menu.buildFromTemplate(mainMenuTempl)
+Menu.setApplicationMenu(mainMenu)
+
---
+++ showotpeng.js
@@ -0,0 +1,88 @@
+
+const electron = require('electron')
+const fs = require('fs')
+
+const otplib = require('otplib')
+
+const ipc = electron.ipcRenderer
+const dialog = electron.remote.dialog
+
+// ░░ ██
+const delayBar = '███████████████'
+//const delayBar = '░░░░░░░░░░░░░░░'
+
+//
+var allUsers = null
+
+//
+var formatedOTP = function(secret) {
+    let code = otplib.authenticator.generate(secret)
+    return code = code.slice(0, 2) + ' ' + code.slice(2, 4) + ' ' + code.slice(4)
+}
+
+//
+var readUserDataAndCreateTable = function() {
+    // ընտրել ֆայլը
+    var ufpath = dialog.showOpenDialog({
+        defaultPath: __dirname,
+        properties: ['openFile'] })
+
+    // կարդալ ֆայլի պարունակությունը
+   let data = fs.readFileSync(ufpath[0], 'utf8')
+   allUsers = JSON.parse(data)
+
+    // աղյուսակի տողի կաղապարը
+    let oneuser = document.getElementById('one-user')
+    // աղյուսակի մարմինը
+    let userlist = document.getElementById('user-list')
+    while (userlist.lastChild)
+        userlist.removeChild(userlist.lastChild)
+
+    for(let ui in allUsers) {
+        let nam = allUsers[ui].name.toLowerCase()
+        let sec = allUsers[ui].secret
+
+        let nu = oneuser.content.cloneNode(true)
+        let chs = nu.firstElementChild.children
+        chs[0].id = 'name-' + nam
+        chs[0].innerText = allUsers[ui].name
+        chs[1].id = 'secret-' + nam
+        chs[1].innerText = sec
+        chs[2].id = 'pass-' + nam
+        chs[2].innerText = formatedOTP(sec)
+        chs[3].id = 'timer-' + nam
+
+        allUsers[ui].otpElem = chs[2]
+        allUsers[ui].timerElem = chs[3]
+
+        allUsers[ui].updateCode = function() {
+            let seconds = Math.floor(Date.now() / 1000);
+            let delay = seconds % 30
+            
+            if(1 > delay)
+                this.otpElem.innerText = formatedOTP(this.secret)
+
+            //this.timerElem.innerText = 30 - delay
+            this.timerElem.innerText = delayBar.slice(0, (30 - delay) / 2)
+        }
+
+        userlist.appendChild(nu)
+    }
+}
+
+//
+ipc.on('read-data-file', readUserDataAndCreateTable)
+
+//
+ipc.on('show-info', () => {
+    let infoBox = document.getElementById('show-information')
+    document.getElementById('si-ok').onclick = function() { infoBox.close() }
+    infoBox.show() 
+})
+
+//
+setInterval(() => {
+    if( allUsers != null )
+        for(let ui of allUsers)
+            ui.updateCode()
+}, 1000)
